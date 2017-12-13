c3164ae2024b4c141cb75da5319472adb1f93f84
David Spies <david.spies@leapyear.io>
2017-12-13 10:23:33 -0800

    Better performance for Day5

--- src/Day5.hs
+++ src/Day5.hs
@@ -1,9 +1,8 @@
+{-# LANGUAGE BangPatterns #-}
+
 module Day5 where
 
-import           Control.Monad.Loops         (whileM_)
-import           Control.Monad.ST            (runST)
-import           Data.STRef.Strict           (modifySTRef, newSTRef, readSTRef,
-                                              writeSTRef)
+import           Control.Monad.ST.Strict     (runST)
 import           Data.Vector.Unboxed         (Vector)
 import qualified Data.Vector.Unboxed         as Vector
 import qualified Data.Vector.Unboxed.Mutable as MVector
@@ -11,16 +10,14 @@ import qualified Data.Vector.Unboxed.Mutable as MVector
 solution1 :: String -> Int
 solution1 s = runST $ do
     v <- Vector.thaw xs
-    iRef <- newSTRef 0
-    countRef <- newSTRef (0 :: Int)
     let lenxs = Vector.length xs
-    whileM_ ((\i -> i >= 0 && i < lenxs) <$> readSTRef iRef) $ do
-      i <- readSTRef iRef
-      jump <- MVector.read v i
-      MVector.write v i (jump + 1)
-      writeSTRef iRef (i + jump)
-      modifySTRef countRef (+ 1)
-    readSTRef countRef
+        loop i !count
+          | i >= 0 && i < lenxs = do
+              jump <- MVector.read v i
+              MVector.write v i (jump + 1)
+              loop (i + jump) (count + 1)
+          | otherwise = return count
+    loop 0 0
   where
     xs :: Vector Int
     xs = Vector.fromList $ map read $ lines s
@@ -28,17 +25,15 @@ solution1 s = runST $ do
 solution2 :: String -> Int
 solution2 s = runST $ do
     v <- Vector.thaw xs
-    iRef <- newSTRef 0
-    countRef <- newSTRef (0 :: Int)
     let lenxs = Vector.length xs
-    whileM_ ((\i -> i >= 0 && i < lenxs) <$> readSTRef iRef) $ do
-      i <- readSTRef iRef
-      jump <- MVector.read v i
-      let newjump = if jump >= 3 then jump - 1 else jump + 1
-      MVector.write v i newjump
-      writeSTRef iRef (i + jump)
-      modifySTRef countRef (+ 1)
-    readSTRef countRef
+        loop i !count
+          | i >= 0 && i < lenxs = do
+              jump <- MVector.read v i
+              let newjump = if jump >= 3 then jump - 1 else jump + 1
+              MVector.write v i newjump
+              loop (i + jump) (count + 1)
+          | otherwise = return count
+    loop 0 0
   where
     xs :: Vector Int
     xs = Vector.fromList $ map read $ lines s
